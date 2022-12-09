7d7a3ca2d9c1f93444e00a266f7a1973d46c0e32
Nick Menzhuber <nickmenzhuber@Nicks-Mac-mini.local>
2022-12-08 21:32:27 -0600

    added session, fixes comment model, gets user routes going

--- server.js
+++ server.js
@@ -4,11 +4,30 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 //const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 //const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Beware the Jabberwock',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+  };
+  
+  app.use(session(sess));
 
 //app.engine('handlebars', hbs.engine);
 //app.set('view engine', 'handlebars');
