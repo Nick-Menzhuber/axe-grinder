e8f743b7768efcc4f75385917efddc1502ff45c3
Code <sangw009@umn.edu>
2022-12-14 15:49:25 -0600

    fixed the login page, can now sign up, log in, and log out

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,11 @@
-var loginFormHandler = async (event) => {
+const loginFormHandler = async (event) => {
   event.preventDefault();
 
   const email = document.querySelector("#email-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
   if (email && password) {
-    const response = await fetch("controllers/api/userRoutes.js", {
+    const response = await fetch("/api/users/login", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
@@ -32,7 +32,7 @@ const signupFormHandler = async (event) => {
       body: JSON.stringify({ name, email, password }),
       headers: { "Content-Type": "application/json" },
     });
-
+console.log(response);
     if (response.ok) {
       document.location.replace("/");
     } else {
