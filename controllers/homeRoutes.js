be5724a163eb6ee36506b8e7d20735858afb0854
Nick Menzhuber <nickmenzhuber@Nicks-Mac-mini.local>
2022-12-15 21:45:02 -0600

    fixes post route finally

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,19 @@ const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
+router.post("/", withAuth, async (req, res) => {
+  console.log('string', req)
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
 router.get("/", async (req, res) => {
   try {
     // Get all posts and JOIN with user data
